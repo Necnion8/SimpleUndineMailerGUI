5683d6227f8ee7ad5ad24a6d51570f398adf4f05
Necnion8 <necnionch@gmail.com>
2022-12-08 12:08:46 +0900

    Impl: コマンドの実装

--- src/main/java/com/gmail/necnionch/myplugin/simpleundinemailergui/bukkit/GUICommand.java
+++ src/main/java/com/gmail/necnionch/myplugin/simpleundinemailergui/bukkit/GUICommand.java
@@ -1,11 +1,17 @@
 package com.gmail.necnionch.myplugin.simpleundinemailergui.bukkit;
 
+import com.gmail.necnionch.myplugin.simpleundinemailergui.bukkit.gui.MainPanel;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class GUICommand implements TabExecutor {
 
@@ -16,12 +22,29 @@ public class GUICommand implements TabExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        return false;
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
+        if (sender instanceof Player) {
+            MainPanel.UIType ui = null;
+            if (args.length >= 1)
+                ui = MainPanel.UITypeNames.get(args[0].toLowerCase(Locale.ROOT));
+
+            if (ui != null) {
+                new MainPanel(((Player) sender), ui).open();
+            } else {
+                new MainPanel(((Player) sender)).open();
+            }
+        }
+        return true;
     }
 
     @Override
-    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, String[] args) {
+        if (args.length == 1) {
+            return Stream.of(MainPanel.UIType.values())
+                    .map(MainPanel.UIType::getCommandName)
+                    .filter(s -> args[0].regionMatches(true, 0, s, 0, args[0].length()))
+                    .collect(Collectors.toList());
+        }
         return Collections.emptyList();
     }
 
