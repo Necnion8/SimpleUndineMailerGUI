a81c1ff8b1ee0d5eb4f272468f674fb0c1e856ec
Necnion8 <necnionch@gmail.com>
2023-05-09 10:16:56 +0900

    Impl: GUIを開くメソッドを追加

--- src/main/java/com/gmail/necnionch/myplugin/simpleundinemailergui/bukkit/MailGUIPlugin.java
+++ src/main/java/com/gmail/necnionch/myplugin/simpleundinemailergui/bukkit/MailGUIPlugin.java
@@ -1,9 +1,15 @@
 package com.gmail.necnionch.myplugin.simpleundinemailergui.bukkit;
 
 import com.gmail.necnionch.myplugin.simpleundinemailergui.bukkit.commands.GUICommand;
+import com.gmail.necnionch.myplugin.simpleundinemailergui.bukkit.gui.MainPanel;
 import com.gmail.necnionch.myplugin.simpleundinemailergui.bukkit.gui.Panel;
+import com.gmail.necnionch.myplugin.simpleundinemailergui.bukkit.gui.geyser.BedrockMailPanel;
 import com.gmail.necnionch.myplugin.simpleundinemailergui.bukkit.hooks.InfoGUIBridge;
 import com.gmail.necnionch.myplugin.simpleundinemailergui.bukkit.hooks.MailWrapper;
+import com.gmail.necnionch.myplugin.simpleundinemailergui.bukkit.util.MailPermission;
+import net.md_5.bungee.api.ChatColor;
+import org.bitbucket.ucchy.undine.sender.MailSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.geysermc.floodgate.api.FloodgateApi;
 import org.geysermc.floodgate.api.player.FloodgatePlayer;
@@ -59,6 +65,29 @@ public final class MailGUIPlugin extends JavaPlugin {
             return FloodgateApi.getInstance().getPlayer(playerId);
         }
         return null;
+    }
+
+
+    public void openGUI(Player player, MainPanel.UIType ui) {
+        if (enabledFloodgate) {
+            FloodgatePlayer fPlayer = getFloodgatePlayer(player.getUniqueId());
+            if (fPlayer != null) {
+                BedrockMailPanel.open(fPlayer, MailSender.getMailSender(player), ui);
+                return;
+            }
+        }
+
+        if (MailPermission.READ.cannot(player)) {
+            player.sendMessage(ChatColor.RED + "メールを見る権限がありません");
+        } else if (ui != null && ui.can(player)) {
+            new MainPanel(player, ui).open();
+        } else {
+            new MainPanel(player, MainPanel.DEFAULT_UI).open();
+        }
+    }
+
+    public void openGUI(Player player) {
+        openGUI(player, MainPanel.DEFAULT_UI);
     }
 
 }
